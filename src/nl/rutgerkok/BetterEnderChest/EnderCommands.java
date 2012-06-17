bee799ceb430cab6c856e94b35e50f6d7bfc8928
Rutger kok <rutger.kok@live.com>
2012-06-17 12:46:49 +0200

    New command: /bec openinv

--- src/nl/rutgerkok/BetterEnderChest/EnderCommands.java
+++ src/nl/rutgerkok/BetterEnderChest/EnderCommands.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class EnderCommands implements CommandExecutor
 {
@@ -47,11 +48,47 @@ public class EnderCommands implements CommandExecutor
 				}
 				return true;
 			}
+			
+			//swapinv command
+			if(args[0].equalsIgnoreCase("swapinv"))
+			{
+				if(!(sender instanceof Player)||plugin.hasPermission((Player) sender, "betterenderchest.command.swapinv", false))
+				{	//swap the Ender chests
+					if(args.length==3)
+					{	//get the inventories
+						Inventory firstInventory = plugin.getEnderChests().getInventory(args[1]);
+						Inventory secondInventory = plugin.getEnderChests().getInventory(args[2]);
+						if(!firstInventory.getViewers().isEmpty()||!secondInventory.getViewers().isEmpty())
+						{	//oh no! They are being viewed!
+							sender.sendMessage(ChatColor.RED+"Error: someone else is currently viewing the inventories. Please try again later.");
+						}
+						else
+						{	//swap them
+							plugin.getEnderChests().setInventory(args[1], secondInventory);
+							plugin.getEnderChests().setInventory(args[2], firstInventory);
+							//unload them (so that they get reloaded with correct titles)
+							plugin.getEnderChests().saveAndUnloadInventory(args[1]);
+							plugin.getEnderChests().saveAndUnloadInventory(args[2]);
+							sender.sendMessage(ChatColor.GREEN+"Succesfully swapped inventories!");
+						}
+					}
+					else
+					{	//open private Ender chest
+						sender.sendMessage(ChatColor.RED+"Correct syntaxis: /"+label+" swapinv <player1> <player2>");
+					}
+				}
+				else
+				{	//show error
+					sender.sendMessage(ChatColor.RED+"No permissions to do this...");
+				}
+				return true;
+			}
 		}
 		
-		
-		sender.sendMessage(ChatColor.YELLOW+"/"+label+" openinv - opens the public Ender inventory");
-		sender.sendMessage(ChatColor.YELLOW+"/"+label+" openinv <player> - opens the Ender inventory of that player");
+		sender.sendMessage(ChatColor.GRAY+"Please note that some commands might not be availible for your rank.");
+		sender.sendMessage(ChatColor.GOLD+"/"+label+" openinv:"+ChatColor.WHITE+" opens the public Ender inventory");
+		sender.sendMessage(ChatColor.GOLD+"/"+label+" openinv <player>:"+ChatColor.WHITE+" opens the Ender inventory of that player");
+		sender.sendMessage(ChatColor.GOLD+"/"+label+" swapinv <player1> <player2>:"+ChatColor.WHITE+" swaps the Ender inventories");
 		return true;
 	}
 }
