ae4536d655bc0d2e59ac75d0630a0b0a8ba1bd94
Rutger kok <rutger.kok@live.com>
2012-08-30 10:14:47 +0200

    Removed unnessary import

--- src/nl/rutgerkok/BetterEnderChest/BetterEnderChest.java
+++ src/nl/rutgerkok/BetterEnderChest/BetterEnderChest.java
@@ -3,7 +3,6 @@ package nl.rutgerkok.BetterEnderChest;
 import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
