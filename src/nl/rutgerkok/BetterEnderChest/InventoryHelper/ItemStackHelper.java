36aff34a87ec0d5421e3549abe23811e341beb1f
Rutger Kok <rutger.kok@live.com>
2012-10-07 17:42:42 +0200

    Make save and load system more flexible
    Dinnerbone has been adding a lot of meta tags (colors, names, etc.), so
    providing support for everything manually is not really doable anymore.

--- src/nl/rutgerkok/BetterEnderChest/InventoryHelper/ItemStackHelper.java
+++ src/nl/rutgerkok/BetterEnderChest/InventoryHelper/ItemStackHelper.java
@@ -1,13 +1,10 @@
 package nl.rutgerkok.BetterEnderChest.InventoryHelper;
 
-import java.util.HashMap;
-
+import net.minecraft.server.NBTTagCompound;
 import net.minecraftwiki.wiki.NBTClass.Tag;
 
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
-
-import tux2.bookapi.Book;
 
 public class ItemStackHelper {
     /**
@@ -28,46 +25,17 @@ public class ItemStackHelper {
         // Check for more data
         if (stackNBT.findTagByName("tag") != null) {
 
-            Tag moreDataNBT = stackNBT.findTagByName("tag");
+            Tag metaDataNBT = stackNBT.findTagByName("tag");
 
-            // Enchantments
-            if (moreDataNBT.findTagByName("ench") != null) {
-                Tag[] enchantmentsNBT = (Tag[]) moreDataNBT.findTagByName("ench").getValue();
-                for (Tag enchantmentNBT : enchantmentsNBT) {
-                    stack.addEnchantment(Enchantment.getById(Integer.parseInt(enchantmentNBT.findTagByName("id").getValue().toString())),
-                            Integer.parseInt(enchantmentNBT.findTagByName("lvl").getValue().toString())); // Should
-                                                                                                          // be
-                                                                                                          // (:
-                }
+            // Add metadata using some CraftBukkit magic
+            try {
+                CraftItemStack craftStack = new CraftItemStack(stack);
+                craftStack.getHandle().tag = NBTHelper.getNMSFromNBTTagCompound(metaDataNBT);
+                stack = craftStack;
+            } catch(NoClassDefFoundError e) {
+                // No craftbukkit, no meta tags!
             }
-
-            // Books
-            if (moreDataNBT.findTagByName("pages") != null) {
-                Book book = new Book(stack);
-
-                // Pages
-                Tag[] pagesNBT = (Tag[]) moreDataNBT.findTagByName("pages").getValue();
-                String[] pages = new String[pagesNBT.length];
-                for (int i = 0; i < pagesNBT.length; i++) {
-                    pages[i] = (String) pagesNBT[i].getValue();
-                }
-                book.addPages(pages);
-
-                // Author
-                if (moreDataNBT.findTagByName("author") != null) {
-                    book.setAuthor((String) moreDataNBT.findTagByName("author").getValue());
-                }
-
-                // Title
-                if (moreDataNBT.findTagByName("title") != null) {
-                    book.setTitle((String) moreDataNBT.findTagByName("title").getValue());
-                }
-
-                // Get our stack back!
-                stack = book.getItemStack();
-
-            }
-
+            
         }
 
         return stack;
@@ -93,65 +61,23 @@ public class ItemStackHelper {
         stackPropertyNBT[4] = new Tag(Tag.Type.TAG_End, null, null);
         Tag stackNBT = new Tag(Tag.Type.TAG_Compound, "", stackPropertyNBT);
 
-        // More data
-        Tag[] moreDataValue = { new Tag(Tag.Type.TAG_End, null, null) };
-        Tag moreDataNBT = new Tag(Tag.Type.TAG_Compound, "tag", moreDataValue);
-
-        // Enchantments
-        HashMap<Enchantment, Integer> enchantments = (HashMap<Enchantment, Integer>) stack.getEnchantments();
-        if (enchantments.size() > 0) {
-
-            // Create a list tag with the enchantments
-            Tag enchantmentListNBT = new Tag("ench", Tag.Type.TAG_Compound);
-
-            // Loop through all the enchantments
-            for (Enchantment enchantment : enchantments.keySet()) {
-                // Create a Tag array
-                Tag[] enchantmentPropertiesNBT = new Tag[3];
-
-                // Fill the Tag array
-                enchantmentPropertiesNBT[0] = new Tag(Tag.Type.TAG_Short, "id", (short) enchantment.getId());
-                enchantmentPropertiesNBT[1] = new Tag(Tag.Type.TAG_Short, "lvl", (short) enchantments.get(enchantment).shortValue());
-                enchantmentPropertiesNBT[2] = new Tag(Tag.Type.TAG_End, null, null);
-
-                // Make a TAG_Compound out of it and add it to the
-                // enchantmentListNBT
-                enchantmentListNBT.addTag(new Tag(Tag.Type.TAG_Compound, null, enchantmentPropertiesNBT));
+        // Add metadata using some CraftBukkit magic
+        if(stack instanceof CraftItemStack) {
+            CraftItemStack craftStack = (CraftItemStack) stack;
+            if(craftStack.getHandle() != null && craftStack.getHandle().tag != null) {
+                NBTTagCompound nmsMetaData = craftStack.getHandle().tag;
+                // For some reason, this line is needed (why would the server forget the name?)
+                nmsMetaData.setName("tag");
+                Tag metaDataNBT = NBTHelper.getNBTFromNMSTagCompound(nmsMetaData);
+                if(((Tag[])metaDataNBT.getValue()).length > 1) {
+                    stackNBT.addTag(metaDataNBT);
+                }
             }
-
-            moreDataNBT.addTag(enchantmentListNBT);
         }
 
-        // Books
-        if (Book.canContainText(stack)) {
-            Book book = new Book(stack);
-
-            // Add the author (if it exists)
-            String author = book.getAuthor();
-            if (author != null)
-                moreDataNBT.addTag(new Tag(Tag.Type.TAG_String, "author", author));
-
-            // Add the title (if it exists)
-            String title = book.getTitle();
-            if (title != null)
-                moreDataNBT.addTag(new Tag(Tag.Type.TAG_String, "title", title));
-
-            // Add the pages
-            // Create a list tag with the pages
-            Tag pagesNBT = new Tag("pages", Tag.Type.TAG_String);
-
-            for (String page : book.getPages()) {
-                pagesNBT.addTag(new Tag(Tag.Type.TAG_String, null, page));
-            }
-
-            moreDataNBT.addTag(pagesNBT);
-        }
-
-        // Add the more data tag if it's not empty
-        if (((Tag[]) moreDataNBT.getValue()).length > 1) {
-            stackNBT.addTag(moreDataNBT);
-        }
-
+        // Return it
         return stackNBT;
     }
+
+
 }
