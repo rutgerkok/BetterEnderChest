a55577655d5229a71928dfe11a38c639c1bd2445
rutgerkok <rutger.kok@live.com>
2014-04-16 09:21:44 +0200

    Update to latest UUIDFetcher

--- src/main/java/nl/rutgerkok/betterenderchest/uuidconversion/UUIDFetcher.java
+++ src/main/java/nl/rutgerkok/betterenderchest/uuidconversion/UUIDFetcher.java
@@ -1,16 +1,15 @@
 /*
  * UUID fetcher by Nate Mortensen.
- * https://gist.github.com/evilmidget38/df8dcd7855937e9d1e1f
+ * https://gist.github.com/evilmidget38/26d70114b834f71fb3b4
  * 
  * Modified by BetterEnderChest to throw less generic exceptions, return
- * ChestOwner instead of UUID, use the new API Mojang provided to avoid getting
- * rate-limited and to add support to continue using names.
+ * ChestOwner instead of UUID and to add support to continue using names.
  */
 package nl.rutgerkok.betterenderchest.uuidconversion;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,22 +25,12 @@ import nl.rutgerkok.betterenderchest.chestowner.ChestOwner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class UUIDFetcher implements Callable<Map<String, ChestOwner>> {
-    private static final String AGENT = "minecraft";
-    private static final int MAX_SEARCH = 100;
-    private static final String PROFILE_URL = "https://api.mojang.com/profiles/" + AGENT;
-
-    private static String buildBody(List<String> names, int startPos) {
-        List<String> lookups = new ArrayList<String>();
-        for (int i = startPos; i < startPos + MAX_SEARCH && i < names.size(); i++) {
-            lookups.add(names.get(i));
-        }
-        return JSONValue.toJSONString(lookups);
-    }
+    private static final String PROFILE_URL = "https://api.mojang.com/profiles/minecraft";
+    private static final double PROFILES_PER_REQUEST = 100;
 
     private static HttpURLConnection createConnection() throws IOException {
         URL url = new URL(PROFILE_URL);
@@ -54,24 +43,33 @@ public class UUIDFetcher implements Callable<Map<String, ChestOwner>> {
         return connection;
     }
 
+    private static UUID getUUID(String id) {
+        return UUID.fromString(id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" + id.substring(20, 32));
+    }
+
     private static void writeBody(HttpURLConnection connection, String body) throws IOException {
-        DataOutputStream writer = new DataOutputStream(connection.getOutputStream());
-        writer.write(body.getBytes());
-        writer.flush();
-        writer.close();
+        OutputStream stream = connection.getOutputStream();
+        stream.write(body.getBytes());
+        stream.flush();
+        stream.close();
     }
 
     private final JSONParser jsonParser = new JSONParser();
 
     private final List<String> names;
     private final BetterEnderChest plugin;
+    private final boolean rateLimiting;
     private final Map<String, ChestOwner> specialChests;
 
-    @SuppressWarnings("deprecation")
-    public UUIDFetcher(BetterEnderChest plugin, Collection<String> batch) {
-        this.plugin = plugin;
+    public UUIDFetcher(BetterEnderChest plugin, Collection<String> names) {
+        this(plugin, names, true);
+    }
 
-        names = new ArrayList<String>(batch);
+    @SuppressWarnings("deprecation")
+    public UUIDFetcher(BetterEnderChest plugin, Collection<String> names, boolean rateLimiting) {
+        this.plugin = plugin;
+        this.names = new ArrayList<String>(names);
+        this.rateLimiting = rateLimiting;
 
         // Move over special chests early
         specialChests = new HashMap<String, ChestOwner>();
@@ -111,20 +109,24 @@ public class UUIDFetcher implements Callable<Map<String, ChestOwner>> {
         }
 
         Map<String, ChestOwner> uuidMap = new HashMap<String, ChestOwner>();
-        for (int i = 0; i < names.size(); i += MAX_SEARCH) {
-            String body = buildBody(names, i);
+        int requests = (int) Math.ceil(names.size() / PROFILES_PER_REQUEST);
+        for (int i = 0; i < requests; i++) {
             HttpURLConnection connection = createConnection();
+            String body = JSONArray.toJSONString(names.subList(i * 100, Math.min((i + 1) * 100, names.size())));
             writeBody(connection, body);
             JSONArray array = (JSONArray) jsonParser.parse(new InputStreamReader(connection.getInputStream()));
-            if (array.size() == 0) {
-                break;
-            }
             for (Object profile : array) {
                 JSONObject jsonProfile = (JSONObject) profile;
                 String id = (String) jsonProfile.get("id");
                 String name = (String) jsonProfile.get("name");
-                UUID uuid = UUID.fromString(id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" + id.substring(20, 32));
+                UUID uuid = UUIDFetcher.getUUID(id);
                 uuidMap.put(name, plugin.getChestOwners().playerChest(name, uuid));
+            }
+            if (rateLimiting && i != requests - 1) {
+                try {
+                    Thread.sleep(100L);
+                } catch (InterruptedException e) {
+                }
             }
         }
 
@@ -133,4 +135,6 @@ public class UUIDFetcher implements Callable<Map<String, ChestOwner>> {
 
         return uuidMap;
     }
+
+    // Remainder of class omitted, methods were not used
 }
